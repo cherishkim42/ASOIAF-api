5dce0b87f103b8aae6f6bb82d160513d39146b3d
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-02-28 18:35:06 -0800

    used basic dummy object to test routes

--- server.js
+++ server.js
@@ -5,9 +5,36 @@ const app = express();
 
 require('dotenv').config();
 
+const dummy = {
+    '1': {
+        'name': ['Aegon Targaryen', 'Jon Snow', 'King in the North'],
+        'house': ['Targaryen', 'Stark'],
+        'allegiance': ['Daenerys Targaryen'],
+        'against': ['Night King', 'Cersei Lannister'],
+        'status': 'alive'
+    },
+    '2': {
+        'id': 2,
+        'name': 'Sansa Stark',
+        'house': ['Stark'],
+        'allegiance': ['Jon Snow'],
+        'against': ['Cersei Lannister', 'Petyr Baelish', 'House Bolton'],
+        'status': 'alive'
+    }
+};
+
 // route route
-app.get('/', (req, res) => {
-    res.send('Whee it works')
+app.get('/', function (req, res) {
+    res.send(dummy)
+});
+
+// return a single profile
+app.get('/:id', function (req, res) {
+    const char = dummy[req.params.id]
+    if (char) {
+        return (res.send(char))
+    }
+    res.send("Sorry! No character profile with that ID exists.")
 });
 
 app.listen(3000, () => {
---
+++ todo.md
@@ -0,0 +1,5 @@
+'/' : root route (landing page)
+'/profiles' : index. see all existing character profiles
+'/profiles/new' : form to make a new profile for a character
+'/profiles/:id' : show a single character profile
+'/profiles/:id/edit' : edit an existing character profile
