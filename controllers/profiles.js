d8adcebea964d916e8dcb1fe776af15ce2d4fda0
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-03-08 16:29:38 -0800

    heroku

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,59 +1,60 @@
+/* eslint-disable no-console */
+/* eslint-disable no-shadow */
 const express = require('express');
+
 const router = express.Router();
 const Profile = require('../models/profile.js');
 
 // INDEX to see all profile documents
-router.get('/profiles', function (req, res) {
-  Profile.find().then(function (profiles) {
+router.get('/profiles', (req, res) => {
+  Profile.find().then((profiles) => {
     res.send(profiles);
   });
 });
 
 // CREATE a new profile document (via Postman, Insomnia)
-router.post('/profiles/new', function (req, res) {
+router.post('/profiles/new', (req, res) => {
   const profile = new Profile(req.body);
-  profile.save().then(function (profile) {
+  profile.save().then((profile) => {
     res.status(200).json(profile);
   });
 });
 
 // SHOW profiles based on a single trait
 router.get('/profiles/trait', (req, res) => {
-  console.log(req.query)
+  console.log(req.query);
   Profile.find(req.query)
     .then((profiles) => {
-      res.send(profiles)
+      res.send(profiles);
     });
 });
 
 // SHOW a single profile document
-router.get('/profiles/:id', function (req, res) {
-  Profile.findById(req.params.id).then(function (profile) {
+router.get('/profiles/:id', (req, res) => {
+  Profile.findById(req.params.id).then((profile) => {
     res.json(profile);
   });
 });
 
-
-
 // UPDATE
-router.put('/profiles/:id/edit', function (req, res) {
+router.put('/profiles/:id/edit', (req, res) => {
   Profile.findByIdAndUpdate(req.params.id, req.body)
-    .then((profile) => {
+    .then(() => {
       res.send({ status: 200 });
     });
 });
 
 // DELETE
-router.delete('/profiles/:id/delete', function (req, res) {
+router.delete('/profiles/:id/delete', (req, res) => {
   Profile.findByIdAndRemove(req.params.id)
-    .then((profile) => {
+    .then(() => {
       res.json({ status: 200 });
     });
 });
 
-//GET Catchall
+// GET Catchall
 router.get('/*', (req, res) => {
-  res.send({ status: 400 })
-})
+  res.send({ status: 400 });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
-const port = process.env.PORT || 3000;
-const AtlasUri = process.env.ATLAS_CONNECTION;
+
+// const port = process.env.PORT || 3000;
 const app = express();
-assert = require('assert');
 
 const profileController = require('./controllers/profiles');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/asoiaf-api');
 mongoose.connect(process.env.ATLAS_CONNECTION, {
-    useNewUrlParser: true
+  useNewUrlParser: true,
 });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
 mongoose.set('debug', true);
@@ -22,7 +22,7 @@ mongoose.set('debug', true);
 app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false,
 }));
 app.use(expressValidator());
 app.use(express.static('public'));
@@ -30,7 +30,7 @@ app.use(express.static('public'));
 app.use('/', profileController);
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000!')
+  console.log('App listening on port 3000!');
 });
 
 module.exports = app;
